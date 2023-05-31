aa4bb1bc8388b5eef15791d76d7649f0b2e12745
Tyler Hampton <thampton92@gmail.com>
2023-05-31 15:16:24 -0400

    Create variables for operators and operator function

--- calcscript.js
+++ calcscript.js
@@ -14,7 +14,23 @@ function divide(a, b) {
     return a / b;
 };
 
-console.log(add(10, 5));
-console.log(subtract(10, 5));
-console.log(multiply(10, 5));
-console.log(divide(10, 5));
+let firstQuantity = null;
+let secondQuantity = null;
+let operator = null;
+
+function operate(operator, firstQuantity, secondQuantity) {
+    if (operator == 'add') {
+        return add(firstQuantity, secondQuantity);
+    } else if (operator == 'subtract') {
+        return subtract(firstQuantity, secondQuantity);
+    } else if (operator == 'multiply') {
+        return multiply(firstQuantity, secondQuantity);
+    } else if (operator == 'divide') {
+        return divide(firstQuantity, secondQuantity);
+    }
+}
+
+console.log(operate('add', 10, 5));
+console.log(operate('subtract', 10, 5));
+console.log(operate('multiply', 10, 5));
+console.log(operate('divide', 10, 5));
